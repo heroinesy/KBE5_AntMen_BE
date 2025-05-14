bd57d8367685c68e032beb68c2426351367ceda2
ikik-pd <ikik.pd@gmail.com>
2025-05-14 16:55:46 +0900

    init

---
+++ src/main/java/com/antmen/antwork/AntMenApplication.java
@@ -0,0 +1,13 @@
+package com.antmen.antwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntMenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntMenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antmen/antwork/AntMenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antmen.antwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntMenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
