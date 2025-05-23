a3c16e1ff77cd89cce76510f61aa1cddc5f789db
Chung-Daeun <daeunchungb2m@gmail.com>
2025-05-23 11:21:08 +0900

    chore: delete root scr & modify gradle(add swagger)

--- src/main/java/com/antmen/antwork/manager/repository/ReviewMateRepo.java
+++
@@ -1,9 +0,0 @@
-package com.antmen.antwork.manager.repository;
-
-import com.antmen.antwork.entity.ReviewMate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewMateRepo extends JpaRepository<ReviewMate,Integer> {
-}
