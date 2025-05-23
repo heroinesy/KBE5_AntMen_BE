62413fc7761578c2a4ba583bd950f1f1b2da6598
Yoon Yerim <yeeefla@naver.com>
2025-05-23 15:29:56 +0900

    매니저 업무 entity 생성

--- m-common/src/main/java/com/antmen/antwork/common/domain/entity/ReservationComment.java
+++ m-common/src/main/java/com/antmen/antwork/common/domain/entity/ReservationComment.java
@@ -1,16 +1,26 @@
 package com.antmen.antwork.common.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "reservation_comment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ReservationComment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long reservation_id;
+    private Long reservationId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "reservation_id", nullable = false)
+    private Reservation reservation;
 
     private LocalDateTime checkinAt;
 
