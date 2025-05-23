e3d400d56c2a39cbd1780f97ccec1cfd96d2f4d1
Chung-Daeun <daeunchungb2m@gmail.com>
2025-05-23 11:07:55 +0900

    chore: delete root scr & modify gradle

---
+++ m-manager/src/main/java/com/antmen/antwork/manager/ManagerApplication.java
@@ -0,0 +1,25 @@
+package com.antmen.antwork.manager;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApplication {
+
+    public static void main(String[] args) {
+        // .env 파일 로드
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")  // 루트 디렉토리의 .env 파일
+                .ignoreIfMissing()
+                .load();
+
+        // 시스템 프로퍼티로 설정
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(ManagerApplication.class, args);
+    }
+
+}
