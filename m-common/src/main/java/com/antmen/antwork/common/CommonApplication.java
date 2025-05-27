8e4f21358d2ce0acbd502eaef20d309224c4105a
ikik-pd <ikik.pd@gmail.com>
2025-05-27 17:39:20 +0900

    chore: m-common bootRun 수정

--- m-common/src/main/java/com/antmen/antwork/common/CommonApplication.java
+++ m-common/src/main/java/com/antmen/antwork/common/CommonApplication.java
@@ -10,14 +10,17 @@ public class CommonApplication {
 	public static void main(String[] args) {
 		// .env 파일 로드
 		Dotenv dotenv = Dotenv.configure()
-				.directory("./")  // 루트 디렉토리의 .env 파일
+				.directory("./") // 루트 디렉토리의 .env 파일
 				.ignoreIfMissing()
 				.load();
 
 		// 시스템 프로퍼티로 설정
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		// 환경변수 값 출력 (확인용)
+		System.out.println("MYSQL_USERNAME: " + System.getProperty("MYSQL_USERNAME"));
+		System.out.println("MYSQL_PASSWORD: " + System.getProperty("MYSQL_PASSWORD"));
+	
 
 		SpringApplication.run(CommonApplication.class, args);
 	}
