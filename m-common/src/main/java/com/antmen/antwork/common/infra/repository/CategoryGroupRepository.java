1720f8a3c8fec65d2f16957a0932ab899574ec90
heroinesy <jane6293@naver.com>
2025-05-27 20:46:42 +0900

    카테고리 그룹 파일 관련 파일 삭제

--- m-common/src/main/java/com/antmen/antwork/common/infra/repository/CategoryGroupRepository.java
+++ m-common/src/main/java/com/antmen/antwork/common/infra/repository/CategoryGroupRepository.java
@@ -1,6 +1,5 @@
 package com.antmen.antwork.common.infra.repository;
 
-import com.antmen.antwork.common.domain.entity.CategoryGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
