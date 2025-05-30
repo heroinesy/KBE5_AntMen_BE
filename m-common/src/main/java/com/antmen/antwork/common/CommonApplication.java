4c75f19288613828913c3090454f0b33907187dc
heroinesy <jane6293@naver.com>
2025-05-30 11:09:21 +0900

    fix: 실행 모듈 경로 추가

--- m-common/src/main/java/com/antmen/antwork/common/CommonApplication.java
+++ m-common/src/main/java/com/antmen/antwork/common/CommonApplication.java
@@ -3,8 +3,12 @@ package com.antmen.antwork.common;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan
+@EnableJpaRepositories
 public class CommonApplication {
 
 	public static void main(String[] args) {
