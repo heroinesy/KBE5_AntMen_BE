c3b512f7e2405dcf8e4b7fbe0093705939919477
Chung-Daeun <daeunchungb2m@gmail.com>
2025-06-05 14:29:02 +0900

    chore: account 필요없는 파일 삭제

--- m-common/src/main/java/com/antmen/antwork/common/infra/repository/account/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.antmen.antwork.common.infra.repository.account;
-
-import com.antmen.antwork.common.domain.entity.account.CustomerDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<CustomerDetail, Long> {
-}
