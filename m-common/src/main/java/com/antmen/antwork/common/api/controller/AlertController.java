178f0b5c9e4683436e673eb89a160a7f4dc9c2a6
Chung-Daeun <daeunchungb2m@gmail.com>
2025-06-11 17:34:42 +0900

    docker-compose.yml 수정

--- m-common/src/main/java/com/antmen/antwork/common/api/controller/AlertController.java
+++ m-common/src/main/java/com/antmen/antwork/common/api/controller/AlertController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +17,7 @@ public class AlertController {
 
     private final AlertService alertService;
 
-    @GetMapping()
+    @GetMapping("")
     public ResponseEntity showAllAlerts(@AuthenticationPrincipal AuthUserDto authUserDto) {
         return ResponseEntity
                 .status(HttpStatus.OK)
