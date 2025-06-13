2de25131ad76999ae518bcb49f525f22bf68d6d3
Chung-Daeun <daeunchungb2m@gmail.com>
2025-06-13 13:06:23 +0900

    주석처리를 잘못함 ㅜㅜ

--- m-common/src/main/java/com/antmen/antwork/common/service/serviceAccount/UserGoogleService.java
+++ m-common/src/main/java/com/antmen/antwork/common/service/serviceAccount/UserGoogleService.java
@@ -23,29 +23,29 @@ public class UserGoogleService {
     @Value("${oauth.google.redirect-uri}")
     private String userGoogleRedirectUri;
 
-//    public UserAccessTokenDto getAccessToken(String code) {
-//        // 인가코드, clientId, client_secret, redirect_uri, grant_type
-//
-//        // spring6부터 RestTemplate 비추천상태이므로 RestClient 사용
-//        RestClient restClient = RestClient.create();
-//
-//        // MaltiValueMap을 통해 자동으로 form-data형식으로 body 조립 가능
-//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//        params.add("code", code);
-//        params.add("client_id", userGoogleClientId);
-//        params.add("client_secret", userGoogleClientSecret);
-//        params.add("redirect_uri", userGoogleRedirectUri);
-//        params.add("grant_type", "authorization_code");
-//
-//        ResponseEntity<UserAccessTokenDto> response = restClient.post()
-//                .uri("https://oauth2.googleapis.com/token")
-//                .header("Content-Type", "application/x-www-form-urlencoded")
-//                .body(params)
-//                .retrieve()// retrieve: 응답 body 값만을 추출
-//                .toEntity(UserAccessTokenDto.class);
-//
-//        return response.getBody();
-//    }
+    public UserAccessTokenDto getAccessToken(String code) {
+        // 인가코드, clientId, client_secret, redirect_uri, grant_type
+
+        // spring6부터 RestTemplate 비추천상태이므로 RestClient 사용
+        RestClient restClient = RestClient.create();
+
+        // MaltiValueMap을 통해 자동으로 form-data형식으로 body 조립 가능
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("code", code);
+        params.add("client_id", userGoogleClientId);
+        params.add("client_secret", userGoogleClientSecret);
+        params.add("redirect_uri", userGoogleRedirectUri);
+        params.add("grant_type", "authorization_code");
+
+        ResponseEntity<UserAccessTokenDto> response = restClient.post()
+                .uri("https://oauth2.googleapis.com/token")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .body(params)
+                .retrieve()// retrieve: 응답 body 값만을 추출
+                .toEntity(UserAccessTokenDto.class);
+
+        return response.getBody();
+    }
 
     public UserGoogleProfileDto getGoogleProfile(String token){
         RestClient restClient = RestClient.create();
