f1287eeb8c5b4f9640f1b09ddc68cfd9bb1156c0
SY <101615063+heroinesy@users.noreply.github.com>
2025-06-20 15:20:46 +0900

    feat: checkin, checkout 기능 구현

---
+++ m-common/src/main/java/com/antmen/antwork/common/service/serviceReservation/ReservationCommentService.java
@@ -0,0 +1,51 @@
+package com.antmen.antwork.common.service.serviceReservation;
+
+import com.antmen.antwork.common.api.request.reservation.CheckInRequestDto;
+import com.antmen.antwork.common.api.request.reservation.CheckOutRequestDto;
+import com.antmen.antwork.common.domain.entity.reservation.Reservation;
+import com.antmen.antwork.common.domain.entity.reservation.ReservationComment;
+import com.antmen.antwork.common.domain.entity.reservation.ReservationStatus;
+import com.antmen.antwork.common.domain.exception.NotFoundException;
+import com.antmen.antwork.common.infra.repository.reservation.ReservationCommentRepository;
+import com.antmen.antwork.common.infra.repository.reservation.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationCommentService {
+    private final ReservationRepository reservationRepository;
+    private final ReservationCommentRepository reservationCommentRepository;
+
+    // 매니저 check-in time update
+    @Transactional
+    public void checkIn(Long reservationId, CheckInRequestDto dto){
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(() -> new NotFoundException("예약을 찾을 수 없습니다."));
+
+        ReservationComment comment = reservationCommentRepository.findById(reservationId)
+                .orElse(ReservationComment.builder()
+                        .reservation(reservation).build());
+
+        comment.setCheckinAt(dto.getCheckinAt());
+        reservationCommentRepository.save(comment);
+    }
+
+    // 매니저 check-out time update
+    @Transactional
+    public void checkOut(Long reservationId, CheckOutRequestDto dto) {
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(() -> new NotFoundException("예약이 존재하지 않습니다."));
+        reservation.setReservationStatus(ReservationStatus.DONE);
+
+
+        ReservationComment comment = reservationCommentRepository.findById(reservationId)
+                .orElseThrow(() -> new NotFoundException("체크인 기록이 없습니다."));
+
+        comment.setCheckoutAt(dto.getCheckoutAt());
+        comment.setComment(dto.getComment());
+        reservationCommentRepository.save(comment);
+    }
+}
--- m-manager/src/main/java/com/antmen/antwork/manager/controller/ManagerReservationController.java
+++ m-manager/src/main/java/com/antmen/antwork/manager/controller/ManagerReservationController.java
@@ -1,8 +1,11 @@
 package com.antmen.antwork.manager.controller;
 
+import com.antmen.antwork.common.api.request.reservation.CheckInRequestDto;
+import com.antmen.antwork.common.api.request.reservation.CheckOutRequestDto;
 import com.antmen.antwork.common.api.request.reservation.ReservationStatusChangeRequestDto;
 import com.antmen.antwork.common.api.response.reservation.ReservationHistoryDto;
 import com.antmen.antwork.common.api.response.reservation.ReservationResponseDto;
+import com.antmen.antwork.common.service.serviceReservation.ReservationCommentService;
 import com.antmen.antwork.common.service.serviceReservation.ReservationService;
 import com.antmen.antwork.common.service.serviceReservation.ReviewService;
 import com.antmen.antwork.common.util.AuthUserDto;
@@ -21,6 +24,7 @@ import java.util.stream.Collectors;
 public class ManagerReservationController {
     private final ReservationService reservationService;
     private final ReviewService reviewService;
+    private final ReservationCommentService reservationCommentService;
 
     /**
      * 매니저 예약 목록 조회
@@ -67,6 +71,23 @@ public class ManagerReservationController {
             @RequestBody ReservationStatusChangeRequestDto dto) {
         Long loginManagerId = authUserDto.getUserIdAsLong();
         reservationService.changeStatusByManager(id, loginManagerId, dto.getStatus());
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}/checkin")
+    public ResponseEntity<Void> checkin(
+            @PathVariable Long id,
+            @RequestBody CheckInRequestDto dto) {
+        reservationCommentService.checkIn(id, dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}/check-out")
+    public ResponseEntity<Void> checkOut(
+            @PathVariable Long id,
+            @RequestBody CheckOutRequestDto dto
+    ) {
+        reservationCommentService.checkOut(id, dto);
         return ResponseEntity.ok().build();
     }
 }
