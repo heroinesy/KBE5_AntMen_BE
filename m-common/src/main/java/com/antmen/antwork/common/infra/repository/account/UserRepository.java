b35986c329eb68557cd8ba9e5ab17fdb986cf76c
Chung-Daeun <daeunchungb2m@gmail.com>
2025-07-01 19:38:33 +0900

    fix: build error 수정

--- m-common/src/main/java/com/antmen/antwork/common/infra/repository/account/UserRepository.java
+++ m-common/src/main/java/com/antmen/antwork/common/infra/repository/account/UserRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
     Optional<User> findByUserLoginId(String userLoginId);
 
     @Query("SELECT u FROM User u " +
